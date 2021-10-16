d5f082f960d8d782faf5454b7228ad3264669aac
SciDroid <scidroidgames@gmail.com>
2021-10-16 06:52:56 +0000

    look now

--- pages/app.js
+++ pages/app.js
@@ -2,11 +2,15 @@ import { useUser } from "@auth0/nextjs-auth0";
 
 export default function app() {
   const { user } = useUser();
-
   if (user) {
     return (
       <>
-        <h1>Hello {user.name}</h1>
+        <h1 className="mb-2 mb-8 text-5xl font-extrabold leading-none md:pl-2">
+          Hello,
+          <span className="inline-block text-deep-purple-accent-400">
+            {user.name}
+          </span>
+        </h1>
       </>
     );
   }
